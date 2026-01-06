798c727ff3582698c9876d0edd750774587523ce
T Srinivasa Rao <tatisettisrinivasarao.22.cse@anits.edu.in>
2026-01-06 21:14:58 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiBaseUrl: 'http://localhost:3000' // Change this to your production API URL when ready
+    apiBaseUrl: 'http://13.61.136.239/' // Change this to your production API URL when ready
 };
